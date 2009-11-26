7980e9e3cd9fc48b206e55aaceb95ac8465d7c7e
Curtis Cooley <curtis@thecooleys.org>
2009-11-25 17:46:28 -0800

    Front page and email cleanup

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
     from "Curtis Cooley <curtis@thecooleys.org>"  
     subject "Welcome to Team Captain"  
     sent_on Time.now 
-    body :user => user, :url => "http://localhost:3000/admin/validate/#{user.registration_key}/#{user.id}"
+    body :user => user, :url => "http://team-manager.heroku.com/admin/validate/#{user.registration_key}/#{user.id}"
   end 
 
   def reminder_email(players, game, user)
