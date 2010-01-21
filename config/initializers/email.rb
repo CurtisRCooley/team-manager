705ba319a5beb41ee387ec562e16402ec3c5c896
Curtis Cooley <curtis@thecooleys.org>
2010-01-20 20:05:31 -0800

    Adding schedules

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,13 +1,14 @@
 # Load mail configuration if not in test environment
+
 if RAILS_ENV != 'test'
   ActionMailer::Base.perform_deliveries = true
   ActionMailer::Base.raise_delivery_errors = true 
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.comcast.net",
+    :port => "25",
+    :domain => "thecooleys.org",
     :user_name => "curtiscooley",
     :password => "Perturbed",
-    :authentication => :login,
-    :port => 25,
-    :domain => "thecooleys.org"
+    :authentication => :login
   }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ ActionController::Routing::Routes.draw do |map|
 
 #  map.resources :players
   map.resources :games
+  map.resources :schedules
 
   # The priority is based upon order of creation: first created -> highest priority.
 
